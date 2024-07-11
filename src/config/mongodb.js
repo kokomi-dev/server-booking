35978b639849f0f162b2a7b09c9be004d5e2cc70
Nguyễn Thế An <nguyenthean12062002@gmail.com>
2024-07-11 22:40:16 +0700

    switch mongodb clound altas

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,2 +1,25 @@
-// nguyenthean12062002
-//TtMhL5ini9Tn6qBr 
+const { MongoClient } = require("mongodb");
+const env = require("~/config/enviroment");
+
+let dataResponseDatabase = null;
+
+const client = new MongoClient(env.MONGODB_URL);
+
+const CONNECT_DB = async () => {
+  try {
+    await client.connect();
+    dataResponseDatabase = client.db(env.DATABASE_NAME);
+  } catch (err) {
+    console.log(err.stack);
+  }
+};
+const GET_DB = () => {
+  if (!dataResponseDatabase) {
+    console.log("No data response from database( Mongo DB)");
+  }
+  return dataResponseDatabase;
+};
+module.exports = {
+  CONNECT_DB,
+  GET_DB,
+};
--- src/server.js
+++ src/server.js
@@ -3,22 +3,34 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 const route = require("./routes/index");
-import errorHandling from "~/middlewares/errorHandling";
-const db = require("./db/db");
-const app = express();
-require("dotenv").config();
-app.use(methodOverride("_method"));
-// Middleware  URL-encoded and JSON
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cors());
+const errorHandling = require("~/middlewares/errorHandling");
+const env = require("~/config/enviroment");
+const { CONNECT_DB, GET_DB } = require("~/config/mongodb");
 
-// Connect to mongodb
-db.connect();
-// route
-route(app);
-app.use(errorHandling);
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running  ${PORT}`);
-});
+const START_SERVICE = () => {
+  const app = express();
+
+  app.use(methodOverride("_method"));
+  // Middleware  URL-encoded and JSON
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(cors());
+  route(app);
+  app.use(errorHandling);
+
+  const PORT = env.PORT || 8080;
+  app.listen(PORT, () => {
+    console.log(`3. Server is running  ${PORT}`);
+  });
+};
+(async () => {
+  try {
+    console.log("1. Connecting mongodb atlas");
+    await CONNECT_DB();
+    console.log("2. Connected to mongodb atlas successfully");
+    START_SERVICE();
+  } catch (error) {
+    console.error(error);
+    process.exit(0);
+  }
+})();
